abffbbd078bda009ec1fa64553697be4740a23d0
praathul <44019979+athulpram@users.noreply.github.com>
2019-01-08 14:09:09 +0530

    Added pause to each cell by clicking on it
    The cell works like a toggle button for each cell
    The green cell represents playing sound red represents paused sound

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-const playSound = function(cellId) {
-  let music = "/sounds/chain1/" + cellId + ".mp3";
-  document.getElementById(cellId).setAttribute("style", "background : green");
+let soundElements = {};
 
-  let audio = new Audio(music);
-  audio.play();
-  audio.onended = function() {
-    document.getElementById(cellId).setAttribute("style", "background : red");
-  };
+let createSoundElement = function(id) {
+  if (soundElements[id] == undefined) {
+    soundElements[id] = {
+      status: false,
+      music: new Audio("/sounds/chain1/" + id + ".mp3")
+    };
+  }
+};
+
+const playSound = function(cellId) {
+  createSoundElement(cellId);
+  playOrStop(cellId);
+  soundElements[cellId].music.onended = () => stopPlaying(cellId);
+};
+
+const stopPlaying = function(id) {
+  soundElements[id].status = false;
+  soundElements[id].music.pause();
+  document.getElementById(id).setAttribute("style", "background : red");
+};
+
+const playOrStop = function(id) {
+  if (soundElements[id].status) {
+    return stopPlaying(id);
+  }
+  soundElements[id].status = true;
+  document.getElementById(id).setAttribute("style", "background : green");
+  soundElements[id].music.play();
 };
